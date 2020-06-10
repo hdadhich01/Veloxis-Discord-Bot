41e93a737e0b8ca7de297de53bc3a56c36ba0602
Harsh Dadhich <hdadhich01@gmail.com>
2020-06-10 15:57:04 -0700

    contains code for player tracking (coming soon; Riot API unreleased)

--- player_statistics.py
+++ player_statistics.py
@@ -1,2 +1,5 @@
-#import discord
-#waiting on tracker.gg
+# this file is used for importing information in main.py on all the valorant player statistics
+# the code is unavilable as valorant hasn't released their API yet allowing us to track player stats
+
+# import discord
+# waiting on tracker.gg
